6ba0c7d3c26845fa1e7cdf1fcc71df88ba23d7b9
Estrella <pl.estrella.b@gmail.com>
2025-03-19 20:21:37 -0400

    subiendo carpetas de algoritmos - p1(hola mundo)

---
+++ problema1/problema1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA");
+            Console.WriteLine("MUNDO");
+            Console.ReadKey();
+        }
+    }
+}
